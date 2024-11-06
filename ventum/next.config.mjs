79eeeff227225d8ac082e579f0b0cf609541208c
Alejandro <alejandrotorres1350@gmail.com>
2024-11-06 09:35:18 -0500

    Configured Sass for styles, added library to add svg icons, and configure aliases

--- ventum/next.config.mjs
+++ ventum/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+      return config;
+    },
+    sassOptions: {
+      additionalData: `$example_var: red;`,
+    }
+  };
+  
+  export default nextConfig;
