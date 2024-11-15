7c483d64f55cfbeabe98393f81547c273722e24b
Jean Pierre Segura <jeanpierresegura@Jean-Pierre-Seguras-MacBookPro.local>
2024-11-14 23:21:52 -0500

    new component quad and fix header : building

--- ventum/app/zonassociales/page.js
+++ ventum/app/zonassociales/page.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import ZonasSocialesHeader from '@/components/ZonasSocialesHeader';
 import MainWrapper from '@/components/MainWrapper';
+import ZonasSocialesQuad from '@/components/ZonasSocialesQuad';
 
 /* -- Styles --*/
 import styles from "@/styles/page.module.scss";
@@ -7,9 +9,13 @@ import styles from "@/styles/page.module.scss";
 const zonassociales = () => {
   return (
     <MainWrapper>
-      <div className={styles.page}>
-        <h1>Zonas Sociales</h1>
-      </div>
+      <ZonasSocialesHeader />
+      <ZonasSocialesQuad 
+        imageSrc="images/home_page_background_2.jpg" /* Replace with your image URL */
+        title="Lobby"
+        subtitle="Doble altura"
+        text="This is some example text content for the section.This is some example text content for the section.This is some example text content for the section.This is some example text content for the section."
+      />
     </MainWrapper>
   )
 }
---
+++ ventum/components/ZonasSocialesHeader/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Image from 'next/image';
+/* -- Styles --*/
+import styles from "./ZonasSocialesHeader.module.scss";
+/* -- Assets --*/
+import ActionArrow from '@/assets/icons/action_arrow.svg';
+import PhoneBackground from '@/assets/icons/phone_background.svg';
+import KnowMoreButton from '@/assets/icons/know_more_button.svg';
+import home_background_1 from '@/assets/images/home_page_background_1.jpg';
+
+
+
+const ZonasSocialesHeader = () => {
+    return (
+        <section className={styles.homeCarousel}>
+            <div className={styles.carouselContent}>
+                <div className={styles.carouselText}>
+                    <h1 className={styles.carouselText_title} >ZONAS SOCIALES</h1>
+                    <h3 className={styles.carouselText_subTitle}>para disfrutar todoso los dias</h3>
+                </div>
+                <div className={styles.arrow_bottom_container}>
+                    <ActionArrow className={styles.arrow_bottom}/>
+                </div>
+                <div className={styles.phone_container}>
+                    <PhoneBackground className={styles.phone_background}/>
+                </div>
+            </div>
+
+            <section className={styles.backgroundImage}>
+                <Image
+                    src={home_background_1}
+                    alt="home_background_1"
+                    fill={true}
+                />
+            </section>
+            
+            
+        </section>
+    );
+};
+export default ZonasSocialesHeader;
